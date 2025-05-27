8c15da6347694a3bfbc2bd3248f5c7ae7dceafa7
Jeremiah Bankole <bankolejeremiahadeoye@gmail.com>
2025-05-27 10:30:09 +0100

    resolved imports error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import PublicRoutes from './routes/PubilcRoutes';
-import PrivateRoutes from "./routes/PrivateRoutes";
+// import PrivateRoutes from "./routes/PrivateRoutes";
 import { useAuth } from './hooks/useAuth'; // custom hook for auth
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
         {!isLoggedIn && <Route path="/*" element={<PublicRoutes />} />}
 
         {/* Private routes for logged-in users */}
-        {isLoggedIn && <Route path="/dashboard/*" element={<PrivateRoutes />} />}
+        {/* {isLoggedIn && <Route path="/dashboard/*" element={<PrivateRoutes />} />} */}
         
         {/* Redirect logged-in users from public routes to dashboard */}
         {isLoggedIn && <Route path="*" element={<Navigate to="/dashboard/home" />} />}
